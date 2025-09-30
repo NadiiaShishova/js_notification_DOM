bb7724150075460fd5ca231a82bf8e18482542a0
Nadiia Shishova <nadin71983@gmail.com>
2025-09-30 23:28:51 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    notification.classList.add("notification", type);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    const h2 = document.createElement("h2");
+    h2.className = "title";
+    h2.textContent = title;
+    const p = document.createElement("p");
+    p.textContent = description;
+    notification.appendChild(h2);
+    notification.appendChild(p);
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        notification.style.visibility = "hidden";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
